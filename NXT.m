41eb06affb40db3bde385140c49258e24fee71b8
Adam Stein <ams593@hotmail.co.uk>
2018-12-04 12:51:50 +0000

    astar working well

--- NXT.m
+++ NXT.m
@@ -1,13 +1,74 @@
 map = [0,0;66,0;66,44;44,44;44,66;110,66;110,110;0,110]; %default map
-goal = [25,25];
-start = [85,85];
+start = [22,88];
+goal = [44,22];
 mainBot = BotSim(map);
 mainBot.drawMap()
-createPointsMap2(mainBot, goal,  start)
 h = init();
-rotateSensor();
+%relocalise([88,88], 0, 36, map)
 astar(map, start, goal, mainBot)
-finish(h)
+%finish(h)
+
+function [weight, optimumAngle] = getWeight(particleScanDistances,SCAN_NUMBER, botSimScanDistances)
+    var = 80;
+        
+    weightForShift = zeros(SCAN_NUMBER, 1);
+    
+    eucledianDistance = sqrt(sum((botSimScanDistances-particleScanDistances).^2));
+    max_weight = (1/sqrt(2*pi*var))*exp(-((eucledianDistance)^2/(2*var)));
+    weight = max_weight;
+    optimumAngle = 0;
+end
+
+function [bestPosX, bestPosY, bestAng] = relocalise(expectedPos, expectedAng, SCAN_NUMBER, map)
+    particles = 1000;
+    posVar = 4;
+    angleVar = 0.2;
+    botSim = BotSim(map);
+    botSim.setBotPos(expectedPos)
+    botSim.setScanConfig(botSim.generateScanConfig(SCAN_NUMBER));
+    [botSimScanDistances1, cp] = botSim.ultraScan();
+    %disp("Correct")
+    %disp(botSimScanDistances1)
+    %scan here
+    [ang_correct,botSimScanDistances] = sensor_rotate(SCAN_NUMBER);
+    %disp("Got")
+    %disp(botSimScanDistances)
+    for i=1:particles
+        mx = normrnd(0, posVar);
+        my = normrnd(0, posVar);
+        ma = normrnd(0, angleVar);
+        newParticlePoint = [expectedPos(1) + mx, expectedPos(2) + my];
+        if(botSim.pointInsideMap(newParticlePoint))
+            simulatedParticle(i) = BotSim(map);
+            simulatedParticle(i).setBotPos(newParticlePoint)
+            simulatedParticle(i).setBotAng(expectedAng + ma);
+            simulatedParticle(i).setScanConfig(simulatedParticle(i).generateScanConfig(SCAN_NUMBER));
+            simulatedParticle(i).drawBot(0);
+            [particleScanDistances, cp] = simulatedParticle(i).ultraScan();  
+            for j=1:SCAN_NUMBER
+                if(botSimScanDistances(j) == 255)
+                    particleScanDistances(j) = 255;
+                end
+                if(botSimScanDistances(j) == -1)
+                    particleScanDistances(j) = -1;
+                end
+            end
+            [max_weight, optimumAngle] = getWeight(particleScanDistances,SCAN_NUMBER, botSimScanDistances);
+            unnormalisedWeights(i) = max_weight;
+            simulatedParticle(i).setBotAng(simulatedParticle(i).getBotAng())
+        end
+    end
+        
+    weight = unnormalisedWeights./sum(unnormalisedWeights);
+    [bestParticleWeight, bestParticleIndex] = max(weight);
+    bestPos = simulatedParticle(bestParticleIndex).getBotPos();
+    bestAng = mod(simulatedParticle(bestParticleIndex).getBotAng(), 2*pi);
+    bestPosX = bestPos(1);
+    bestPosY = bestPos(2);
+    disp("Best Ang")
+    disp(bestAng/(pi) * 180)
+end
+
 
 function astar(map, start, goal, mainBot)
     limsMin = min(map);
@@ -20,16 +81,24 @@ function astar(map, start, goal, mainBot)
 
     pointsMap = createPointsMap2(mainBot, goal, start);
     
-    startIndex = findNearestIndex(start, pointsMap);
+    startIndex = convergeToStart(pointsMap, start, 0);
+    %startIndex = findNearestIndex(start, pointsMap);
     goalIndex = findNearestIndex(goal, pointsMap);
     
     path = calcPath(startIndex, goalIndex, pointsMap, numberOfPoints, pointGridResolution);
-    
     for i = 1:length(path)
         disp(pointsMap(path(i)).coord)
     end
-    currentAng = moveToGoal(mainBot, path, pointsMap, pointGridResolution);
-              
+    currentAng = moveToGoal(path, pointsMap,map);
+    %add localise again
+    
+    %move to goal
+    currentPos = pointsMap(path(length(path) - 1)).coord;
+    nextPos = goal;
+    currentAng = changeAng(currentAng, currentPos, nextPos);
+    d = calcDist(currentPos, nextPos);
+    
+    move(d)
     %mainBot.move(dist);
     %mainBot.drawBot(0);
         
@@ -39,16 +108,20 @@ function d = calcDist(start, goal)
     d = sqrt((goal(2)-start(2))^2 + (goal(1)-start(1))^2);
 end
 
-function startIndex = convergeToStart(robot, pointsMap, bestPos, bestAng)
+function startIndex = convergeToStart(pointsMap, bestPos, bestAng)
     
     pointsMapIndex = findNearestIndex(bestPos, pointsMap);
+    disp(pointsMap(pointsMapIndex).coord)
     
-    angle = changeAng(robot, bestAng, bestPos, pointsMap(pointsMapIndex).coord);
+    angle = changeAng(bestAng, bestPos, pointsMap(pointsMapIndex).coord);
     dist = calcDist(bestPos, pointsMap(pointsMapIndex).coord);
-
+    %disp(angle)
     startIndex = pointsMapIndex;
-    robot.move(dist);
-    robot.turn(-angle);
+    move(dist)
+    turn(-angle)
+    
+    %robot.move(dist);
+    %robot.turn(-angle);
 end
 
 function nearestIndex = findNearestIndex(coord, pointsMap)
@@ -80,8 +153,8 @@ end
 function indexes = findAdjIndexes(index, numberOfPoints)
     if index == 1
         indexes = [2];
-    elseif index == 6
-        indexes = [5];
+    elseif index == 8
+        indexes = [7];
     else 
         indexes = [index - 1 , index + 1];
     end
@@ -139,33 +212,13 @@ function g = gScoreForPoint(pointsMap, adjIndex, res, currentIndex, numberOfPoin
     g = pointsMap(currentIndex).gscore + moveCost;
 end
 
-function paddedPointsMap = applyPad(pointsMap, numberOfPoints)
-    for i = 1:length(pointsMap)
-        if(pointsMap(i).canTraverse == 1)
-           adjIndexes = findAdjIndexes(i, numberOfPoints);
-           pointsMap(i).canTraverseTemp = 1;
-           for j = 1:length(adjIndexes)
-               if(pointsMap(adjIndexes(j)).canTraverse == 0)
-                   pointsMap(i).canTraverseTemp = 0;
-               end
-           end
-        else
-            pointsMap(i).canTraverseTemp = 0;
-        end
-    end
-    for i = 1:length(pointsMap)
-        pointsMap(i).canTraverse = pointsMap(i).canTraverseTemp;
-    end
-    paddedPointsMap = pointsMap;
-end
-
 
 function pointsMap = createPointsMap2(robot, goal,  bestPos)
     index = 1;
     i = 1;
-    x1 = 25;
-    y1 = 25;
-    positions = [[x1,y1], [x1,y1 + 20], [x1,y1 + 40], [x1,y1 + 60],[x1 + 30, y1 + 60], [x1 + 60,y1 + 60]];
+    x1 = 22;
+    y1 = 22;
+    positions = [[x1 + 22,y1], [x1,y1], [x1,y1 + 22], [x1,y1 + 44], [x1,y1 + 66],[x1 + 22, y1 + 66], [x1 + 44,y1 + 66], [x1 + 66,y1 + 66]];
     while i < length(positions)
         p1 = positions(i);
         p2 = positions(i + 1);
@@ -185,28 +238,28 @@ function point = createPoint(robot, goal,bestPos, res, position)
     point.canTraverse = robot.pointInsideMap(position);
 end
 
-function angle = changeAng(robot, currentAng, currentPos, nextPos)
-    angle = atan2((nextPos(2)-currentPos(2)),(nextPos(1)-currentPos(1)));
+function angle = changeAng(currentAng, currentPos, nextPos)
+    angle = mod(atan2((nextPos(2)-currentPos(2)),(nextPos(1)-currentPos(1))), 2*pi);
+    disp("turn")
+    disp((angle - currentAng)/ pi *180)
     if angle ~= currentAng
-        turn(angle - currentAng);
+        turn(-angle + currentAng);
     end
 end
 
-function currentAng = moveToGoal(robot, path, pointsMap, res)
+function currentAng = moveToGoal(path, pointsMap, map)
     currentAng = 0;
+    disp(currentAng)
     currentPos = pointsMap(path(1)).coord;
-    for i = 1:length(path)
+    for i = 2:length(path)
         nextPos = pointsMap(path(i)).coord;
-        currentAng = changeAng(robot, currentAng, currentPos, nextPos);
-        straightMove = mod(currentAng, pi/2) == 0;
-        if straightMove
-            move(res);
-        else
-            move(diagDistance(res));           
-        end
-        
-        robot.drawBot(0);
-        currentPos = nextPos;      
+        currentAng = changeAng(currentAng, currentPos, nextPos);
+        d = calcDist(currentPos, nextPos);
+        move(d)
+        [bestPosX, bestPosY, currentAng] = relocalise(nextPos, currentAng, 36, map);
+        currentPos = [bestPosX, bestPosY];   
+        disp("Current")
+        disp(currentPos)
     end
 end
 
@@ -216,66 +269,106 @@ end
 function move(d)
     rotation = round((d/13.21) * 360);
     motor = NXTMotor('AC') ;
-    motor.Power = 100; 
+    motor.Power = -100; 
     motor.TachoLimit = rotation;
     motor.SendToNXT()
     motor.WaitFor()
 end
+
 function turn(ang)
-    rotation = round(ang/(2*pi)*1000);
+    disp(ang)
+    if ang ~= 0
+        if(ang > pi)
+            ang = pi - ang;
+        end
+        if(ang < -pi)
+            ang = -pi - ang;
+        end
+    rotation = round(ang/(2*pi)*800);
     
     motor = NXTMotor('A') ;
     motor2 = NXTMotor('C') ;
-    disp(ang)
-    disp(rotation)
+    %disp(ang)
+    %disp(rotation)
     if (rotation > 0)
-        motor.Power = -50; 
-        motor2.Power = 50; 
-    else
         motor.Power = 50; 
-        motor2.Power = -50;
+        motor2.Power = -50; 
+        
+    else
+        motor.Power = -50; 
+        motor2.Power = 50;
         rotation = abs(rotation);
     end
-    disp(rotation)
+    
+    %disp(rotation)
     motor.TachoLimit = rotation;
     motor2.TachoLimit = rotation;
     motor.SendToNXT()
     motor2.SendToNXT()
     motor.WaitFor()
     motor2.WaitFor()
-    
+    end
 end
 
 function h = init()
     COM_CloseNXT all %Close any left over connections to the NXT
     h =COM_OpenNXT(); %Connect to the NXT Brick via USB
     COM_SetDefaultNXT(h); %Set our connection as the default
-    OpenUltrasonic(SENSOR_4)
+    OpenUltrasonic(SENSOR_1)
 end
 
-function rotateSensor()
-    d = GetUltrasonic(SENSOR_4);
-    disp(d)
-    scanNumber = 4;
-    motor = NXTMotor('B') ;
-    motor.Power = 100;
-    rotation = round(360/ scanNumber);
-    totalRotation = rotation * (scanNumber -1);
-    for i=1:scanNumber -1
-        motor.TachoLimit = rotation;
-        motor.SendToNXT()
-        motor.WaitFor()
-        d = GetUltrasonic(SENSOR_4);
-        disp(d)
+function [ang_correct,readings] = sensor_rotate(num_scan)
+d = GetUltrasonic(SENSOR_1);
+d = GetUltrasonic(SENSOR_1);
 
+readings = zeros(num_scan,1);
+readings(1,1) = d;
+
+drivingPower = 30;
+returnPower = -50;
+dist  = round(360/num_scan) * (num_scan - 1); % in degrees
+sensor_motor = MOTOR_B;
+
+%define motor to tuen sensor
+m_sensor = NXTMotor(sensor_motor, 'Power', drivingPower);
+m_sensor.TachoLimit = dist;
+
+% prepare motor
+m_sensor.Stop('off');
+m_sensor.ResetPosition();
+
+data = m_sensor.ReadFromNXT();
+pos  = data.Position;
+m_sensor.SendToNXT()
+num = 1;
+Degree = round(360/num_scan);
+diff = round(360/num_scan);
+while pos <= dist + 2
+    data = m_sensor.ReadFromNXT();
+    pos  = data.Position;
+    if pos == Degree 
+        num = num + 1;
+        readings(num,1) = GetUltrasonic(SENSOR_1);
+        Degree = Degree + diff;
     end
-    motor.Power = -100;
-    motor.TachoLimit = totalRotation;
-    motor.SendToNXT()
-    motor.WaitFor()
     
+    if num == num_scan
+        break
+    end
 end
+readings=readings(1:num_scan);
+m_sensor.WaitFor()
+% rotate_lim = dist * (num_scan -1) + pos;
+% m_sensor.TachoLimit = rotate_lim;
 
-function finish(h)
-    COM_CloseNXT(h)
+%define motor to return to original position
+sensor_return = NXTMotor(sensor_motor, 'Power', returnPower);
+data = sensor_return.ReadFromNXT();
+pos  = data.Position;
+sensor_return.TachoLimit = pos;
+
+sensor_return.SendToNXT()
+sensor_return.WaitFor()
+[~,smallest] = min(readings);
+ang_correct = (smallest-1)/num_scan* 360;
 end
